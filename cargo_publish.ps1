195890371ae40b6fd2e0466fd2c15cdb63adca8d
Shunsuke Shibayama <sbym1346@gmail.com>
2023-02-24 18:49:47 +0900

    update upload scripts

--- cargo_publish.ps1
+++ cargo_publish.ps1
@@ -1,14 +1,21 @@
 if ($PWD.Path -eq "$HOME\GitHub\pylyzer") {
-    cd crates/py2erg
-    echo "publish py2erg ..."
-    cargo publish
-    # from cargo 1.66 timeout is not needed
-    # timeout 12
-    cd ../../
-    cargo publish
+    if ($null -eq $env:PYPI_PASSWORD) {
+        echo "set PYPI_PASSWORD environment variable"
+        exit
+    }
+    if ($args[0] -ne "--pip-only") {
+        cd crates/py2erg
+        echo "publish py2erg ..."
+        cargo publish
+        # from cargo 1.66 timeout is not needed
+        # timeout 12
+        cd ../../
+        cargo publish
+    }
     maturin build --release
     $ver = cat Cargo.toml | rg "^version =" | sed -r 's/^version = "(.*)"/\1/'
-    python -m twine upload "target/wheels/pylyzer-$ver-py3-none-win_amd64.whl" -u mtshiba -p $env:PYPI_PASSWORD
+    $whl = "target/wheels/$(ls target/wheels | Select-Object -ExpandProperty Name | rg "pylyzer-$ver")"
+    python -m twine upload $whl -u mtshiba -p $env:PYPI_PASSWORD
     echo "completed"
 } else {
     echo "use this command in the project root"
--- cargo_publish.sh
+++ cargo_publish.sh
@@ -1,14 +1,20 @@
 if [ "$PWD" = "$HOME/github/pylyzer" ]; then
-    cd crates/py2erg
-    echo "publish py2erg ..."
-    cargo publish
-    # from cargo 1.66 timeout is not needed
-    # timeout 12
-    cd ../../
-    cargo publish
+    if [ "$PYPI_PASSWORD" = "" ]; then
+        echo "set PYPI_PASSWORD"
+        exit 1
+    fi
+    if [ "$1" != "--pip-only" ]; then
+        cd crates/py2erg
+        echo "publish py2erg ..."
+        cargo publish
+        # from cargo 1.66 timeout is not needed
+        # timeout 12
+        cd ../../
+        cargo publish
+    fi
     maturin build --release
     ver=`cat Cargo.toml | rg "^version =" | sed -r 's/^version = "(.*)"/\1/'`
-    whl=`ls target/wheels | rg "pylyzer-$ver"`
+    whl=target/wheels/`ls target/wheels | rg "pylyzer-$ver"`
     python3 -m twine upload $whl -u mtshiba -p $PYPI_PASSWORD
     echo "completed"
 else
