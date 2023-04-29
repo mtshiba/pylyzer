964a3d12841d995baed48000c0339c508cdf3f9a
Shunsuke Shibayama <sbym1346@gmail.com>
2023-04-29 12:36:29 +0900

    CI: update scripts

--- cargo_publish.sh
+++ cargo_publish.sh
@@ -1,21 +1,21 @@
 if [[ "$PWD" == */pylyzer ]]; then
-    if [ "$PYPI_PASSWORD" = "" ]; then
-        echo "set PYPI_PASSWORD"
-        exit 1
-    fi
     if [ "$1" != "--pip-only" ]; then
         cd crates/py2erg
         echo "publish py2erg ..."
         cargo publish
-        # from cargo 1.66 timeout is not needed
-        # timeout 12
         cd ../../
         cargo publish
     fi
-    maturin build --release
-    ver=`cat Cargo.toml | rg "^version =" | sed -r 's/^version = "(.*)"/\1/'`
-    whl=target/wheels/`ls target/wheels | rg "pylyzer-$ver"`
-    python3 -m twine upload $whl -u mtshiba -p $PYPI_PASSWORD
+    if [ "$1" != "--cargo-only" ]; then
+        if [ "$PYPI_PASSWORD" = "" ]; then
+            echo "set PYPI_PASSWORD"
+            exit 1
+        fi
+        maturin build --release
+        ver=`cat Cargo.toml | rg "^version =" | sed -r 's/^version = "(.*)"/\1/'`
+        whl=target/wheels/`ls target/wheels | rg "pylyzer-$ver"`
+        python3 -m twine upload $whl -u mtshiba -p $PYPI_PASSWORD
+    fi
     echo "completed"
 else
     echo "use this command in the project root"
