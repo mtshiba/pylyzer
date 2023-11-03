2f919b2e6f292cb0b4c746c2f9d82b379084114c
Shunsuke Shibayama <sbym1346@gmail.com>
2023-11-03 17:20:39 +0900

    change checker: ASTLowerer -> PackageBuilder

--- src/analyze.rs
+++ src/analyze.rs
@@ -2,36 +2,63 @@ use erg_common::config::ErgConfig;
 use erg_common::error::{ErrorCore, ErrorKind, MultiErrorDisplay};
 use erg_common::style::colors::{BLUE, GREEN, RED, YELLOW};
 use erg_common::style::RESET;
-use erg_common::traits::{ExitStatus, Runnable, Stream};
+use erg_common::traits::{New, ExitStatus, Runnable, Stream};
 use erg_common::Str;
+use erg_compiler::GenericHIRBuilder;
 use erg_compiler::artifact::{BuildRunnable, Buildable, CompleteArtifact, IncompleteArtifact};
+use erg_compiler::build_package::GenericPackageBuilder;
 use erg_compiler::context::register::CheckStatus;
 use erg_compiler::context::ModuleContext;
 use erg_compiler::erg_parser::ast::{Module, AST};
+use erg_compiler::erg_parser::build_ast::ASTBuildable;
 use erg_compiler::erg_parser::error::{
-    CompleteArtifact as ParseArtifact, IncompleteArtifact as IncompleteParseArtifact, ParseErrors,
+    CompleteArtifact as ParseArtifact, IncompleteArtifact as IncompleteParseArtifact, ParseErrors, ParserRunnerErrors,
 };
 use erg_compiler::erg_parser::parse::Parsable;
 use erg_compiler::error::{CompileError, CompileErrors};
-use erg_compiler::lower::ASTLowerer;
 use erg_compiler::module::SharedCompilerResource;
 use py2erg::{dump_decl_er, reserve_decl_er, ShadowingMode};
-use rustpython_ast::source_code::RandomLocator;
+use rustpython_ast::source_code::{RandomLocator, SourceRange};
 use rustpython_ast::{Fold, ModModule};
 use rustpython_parser::{Parse, ParseErrorType};
 
 use crate::handle_err;
 
-pub struct SimplePythonParser {}
+#[derive(Debug, Default)]
+pub struct SimplePythonParser {
+    cfg: ErgConfig,
+}
 
 impl Parsable for SimplePythonParser {
     fn parse(code: String) -> Result<ParseArtifact, IncompleteParseArtifact<Module, ParseErrors>> {
-        let py_program = ModModule::parse(&code, "<stdin>").map_err(|_err| ParseErrors::empty())?;
-        let mut locator = RandomLocator::new(&code);
-        // let mut locator = LinearLocator::new(&code);
-        let py_program = locator
-            .fold(py_program)
-            .map_err(|_err| ParseErrors::empty())?;
+        let mut slf = Self::new(ErgConfig::string(code.clone()));
+        slf.build_ast(code)
+            .map(|art| {
+                ParseArtifact::new(art.ast.module, art.warns.into())
+            })
+            .map_err(|iart| {
+                IncompleteParseArtifact::new(
+                    iart.ast.map(|art| art.module),
+                    iart.errors.into(),
+                    iart.warns.into(),
+                )
+            })
+    }
+}
+
+impl New for SimplePythonParser {
+    fn new(cfg: ErgConfig) -> Self {
+        Self { cfg }
+    }
+}
+
+impl ASTBuildable for SimplePythonParser {
+    fn build_ast(
+        &mut self,
+        code: String,
+    ) -> Result<ParseArtifact<AST, ParserRunnerErrors>, IncompleteParseArtifact<AST, ParserRunnerErrors>> {
+        let filename = self.cfg.input.filename();
+        let py_program = self.parse_py_code(code)?;
         let shadowing = if cfg!(feature = "debug") {
             ShadowingMode::Visible
         } else {
@@ -39,32 +66,68 @@ impl Parsable for SimplePythonParser {
         };
         let converter = py2erg::ASTConverter::new(ErgConfig::default(), shadowing);
         let IncompleteArtifact{ object: Some(erg_module), errors, warns } = converter.convert_program(py_program) else { unreachable!() };
+        let erg_ast = AST::new(erg_common::Str::rc(&filename), erg_module);
         if errors.is_empty() {
-            Ok(ParseArtifact::new(erg_module, warns.into()))
+            Ok(ParseArtifact::new(erg_ast, warns.into()))
         } else {
             Err(IncompleteParseArtifact::new(
-                Some(erg_module),
-                warns.into(),
+                Some(erg_ast),
                 errors.into(),
+                warns.into(),
             ))
         }
+    }
+}
+
+impl SimplePythonParser {
+    pub fn parse_py_code(&self, code: String) -> Result<ModModule<SourceRange>, IncompleteParseArtifact<AST, ParserRunnerErrors>>{
+        let py_program = ModModule::parse(&code, "<stdin>").map_err(|err| {
+            let mut locator = RandomLocator::new(&code);
+            // let mut locator = LinearLocator::new(&py_code);
+            let err = locator.locate_error::<_, ParseErrorType>(err);
+            let msg = err.to_string();
+            let loc = err.location.unwrap();
+            let core = ErrorCore::new(
+                vec![],
+                msg,
+                0,
+                ErrorKind::SyntaxError,
+                erg_common::error::Location::range(
+                    loc.row.get(),
+                    loc.column.to_zero_indexed(),
+                    loc.row.get(),
+                    loc.column.to_zero_indexed(),
+                ),
+            );
+            let err = CompileError::new(core, self.cfg.input.clone(), "".into());
+            IncompleteParseArtifact::new(None, ParserRunnerErrors::from(err), ParserRunnerErrors::empty())
+        })?;
+        let mut locator = RandomLocator::new(&code);
+        // let mut locator = LinearLocator::new(&code);
+        let module = locator
+            .fold(py_program)
+            .map_err(|_err| ParserRunnerErrors::empty())?;
+        Ok(module)
     }
 }
 
 #[derive(Debug, Default)]
 pub struct PythonAnalyzer {
     pub cfg: ErgConfig,
-    checker: ASTLowerer,
+    checker: GenericPackageBuilder<SimplePythonParser, GenericHIRBuilder<SimplePythonParser>>,
+}
+
+impl New for PythonAnalyzer {
+    fn new(cfg: ErgConfig) -> Self {
+        let checker = GenericPackageBuilder::new(cfg.clone(), SharedCompilerResource::new(cfg.clone()));
+        Self { checker, cfg }
+    }
 }
 
 impl Runnable for PythonAnalyzer {
     type Err = CompileError;
     type Errs = CompileErrors;
     const NAME: &'static str = "Python Analyzer";
-    fn new(cfg: ErgConfig) -> Self {
-        let checker = ASTLowerer::new(cfg.clone());
-        Self { checker, cfg }
-    }
     #[inline]
     fn cfg(&self) -> &ErgConfig {
         &self.cfg
@@ -95,17 +158,30 @@ impl Buildable for PythonAnalyzer {
         let mod_name = Str::rc(&cfg.input.file_stem());
         Self {
             cfg: cfg.copy(),
-            checker: ASTLowerer::new_with_cache(cfg, mod_name, shared),
+            checker: GenericPackageBuilder::new_with_cache(cfg, mod_name, shared),
+        }
+    }
+    fn inherit_with_name(cfg: ErgConfig, mod_name: Str, shared: SharedCompilerResource) -> Self {
+        Self {
+            cfg: cfg.copy(),
+            checker: GenericPackageBuilder::new_with_cache(cfg, mod_name, shared),
         }
     }
     fn build(&mut self, code: String, mode: &str) -> Result<CompleteArtifact, IncompleteArtifact> {
         self.analyze(code, mode)
     }
+    fn build_from_ast(
+        &mut self,
+        ast: AST,
+        mode: &str,
+    ) -> Result<CompleteArtifact<erg_compiler::hir::HIR>, IncompleteArtifact<erg_compiler::hir::HIR>> {
+        self.check(ast, CompileErrors::empty(), CompileErrors::empty(), mode)
+    }
     fn pop_context(&mut self) -> Option<ModuleContext> {
-        self.checker.pop_mod_ctx()
+        self.checker.pop_context()
     }
     fn get_context(&self) -> Option<&ModuleContext> {
-        Some(self.checker.get_mod_ctx())
+        self.checker.get_context()
     }
 }
 
@@ -113,53 +189,15 @@ impl BuildRunnable for PythonAnalyzer {}
 
 impl PythonAnalyzer {
     pub fn new(cfg: ErgConfig) -> Self {
-        Runnable::new(cfg)
+        New::new(cfg)
     }
 
-    pub fn analyze(
-        &mut self,
-        py_code: String,
-        mode: &str,
-    ) -> Result<CompleteArtifact, IncompleteArtifact> {
-        let filename = self.cfg.input.filename();
-        let py_program = ModModule::parse(&py_code, &filename).map_err(|err| {
-            let mut locator = RandomLocator::new(&py_code);
-            // let mut locator = LinearLocator::new(&py_code);
-            let err = locator.locate_error::<_, ParseErrorType>(err);
-            let msg = err.to_string();
-            let loc = err.location.unwrap();
-            let core = ErrorCore::new(
-                vec![],
-                msg,
-                0,
-                ErrorKind::SyntaxError,
-                erg_common::error::Location::range(
-                    loc.row.get(),
-                    loc.column.to_zero_indexed(),
-                    loc.row.get(),
-                    loc.column.to_zero_indexed(),
-                ),
-            );
-            let err = CompileError::new(core, self.cfg.input.clone(), "".into());
-            IncompleteArtifact::new(None, CompileErrors::from(err), CompileErrors::empty())
-        })?;
-        let mut locator = RandomLocator::new(&py_code);
-        // let mut locator = LinearLocator::new(&py_code);
-        let py_program = locator.fold(py_program).unwrap();
-        let shadowing = if cfg!(feature = "debug") {
-            ShadowingMode::Visible
-        } else {
-            ShadowingMode::Invisible
-        };
-        let converter = py2erg::ASTConverter::new(self.cfg.copy(), shadowing);
-        let IncompleteArtifact{ object: Some(erg_module), mut errors, mut warns } = converter.convert_program(py_program) else { unreachable!() };
-        let erg_ast = AST::new(erg_common::Str::rc(&filename), erg_module);
-        erg_common::log!("AST:\n{erg_ast}");
-        match self.checker.lower(erg_ast, mode) {
+    fn check(&mut self, erg_ast: AST, mut errors: CompileErrors, mut warns: CompileErrors, mode: &str) -> Result<CompleteArtifact, IncompleteArtifact> {
+        match self.checker.build_module(erg_ast, mode) {
             Ok(mut artifact) => {
                 artifact.warns.extend(warns);
                 artifact.warns =
-                    handle_err::filter_errors(self.checker.get_mod_ctx(), artifact.warns);
+                    handle_err::filter_errors(self.get_context().unwrap(), artifact.warns);
                 if errors.is_empty() {
                     Ok(artifact)
                 } else {
@@ -172,12 +210,39 @@ impl PythonAnalyzer {
             }
             Err(iart) => {
                 errors.extend(iart.errors);
-                let errors = handle_err::filter_errors(self.checker.get_mod_ctx(), errors);
+                let errors = handle_err::filter_errors(self.get_context().unwrap(), errors);
                 warns.extend(iart.warns);
-                let warns = handle_err::filter_errors(self.checker.get_mod_ctx(), warns);
+                let warns = handle_err::filter_errors(self.get_context().unwrap(), warns);
                 Err(IncompleteArtifact::new(iart.object, errors, warns))
             }
         }
+    }
+
+    pub fn analyze(
+        &mut self,
+        py_code: String,
+        mode: &str,
+    ) -> Result<CompleteArtifact, IncompleteArtifact> {
+        let filename = self.cfg.input.filename();
+        let parser = SimplePythonParser::new(self.cfg.copy());
+        let py_program = parser.parse_py_code(py_code)
+            .map_err(|iart| {
+                IncompleteArtifact::new(
+                    None,
+                    iart.errors.into(),
+                    iart.warns.into(),
+                )
+            })?;
+        let shadowing = if cfg!(feature = "debug") {
+            ShadowingMode::Visible
+        } else {
+            ShadowingMode::Invisible
+        };
+        let converter = py2erg::ASTConverter::new(self.cfg.copy(), shadowing);
+        let IncompleteArtifact{ object: Some(erg_module), errors, warns } = converter.convert_program(py_program) else { unreachable!() };
+        let erg_ast = AST::new(erg_common::Str::rc(&filename), erg_module);
+        erg_common::log!("AST:\n{erg_ast}");
+        self.check(erg_ast, errors, warns, mode)
     }
 
     pub fn run(&mut self) {
