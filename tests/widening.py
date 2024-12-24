c985e798f08a233fe1d70bffff5d784cf7360524
Shunsuke Shibayama <sbym1346@gmail.com>
2024-12-25 02:52:25 +0900

    build: update deps

--- tests/widening.py
+++ tests/widening.py
@@ -3,3 +3,8 @@ if True:
     b = True
 if True:
     b = "a" # ERR
+
+counter = 100 # counter: Literal[100]
+while counter > 0:
+    counter -= 1  # counter: Int
+    counter -= 1.0  # counter: Float
